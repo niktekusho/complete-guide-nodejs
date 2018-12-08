e30afbf7aca2c7d317a56cf548f48f64fff5dc69
Nicola <nicola.dalmaso@outlook.com>
2018-12-08 19:59:45 +0100

    Lecture 73

--- 5-from-56/app.js
+++ 5-from-56/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const bodyParser = require('body-parser');
 const express = require('express');
 
@@ -5,9 +7,13 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoute = require('./routes/404');
 
+const paths = require('./util/paths');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(paths.basePath(), 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
