babba6a047f9a2b4c76a999cd210a02d32946684
Nicola <nicola.dalmaso@outlook.com>
2019-01-03 16:30:33 +0100

    Module 7 (MVC)
    Controllers

---
+++ modules/module-7/app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoute = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const errorController = require('./controllers/error');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoute);
+app.use(shopRoutes);
+
+app.use(errorController.get404Page);
+
+app.listen(3000);
---
+++ modules/module-7/routes/admin.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+// /admin/add-product => GET
+router.get('/add-product', productsController.getAddProductPage);
+
+// /admin/add-product => POST
+router.post('/add-product', productsController.postNewProduct);
+
+module.exports = router;
